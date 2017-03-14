0b3b77582a66689b01055b111b225147fb148c5c
Gyanstack <support@gyanstack.com>
2017-03-14 19:20:56 +0530

    --- implemented slider ui and google search

--- src/app/advertisment/advertisment.component.ts
+++ src/app/advertisment/advertisment.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-advertisment',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvertismentComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private sanitizer: DomSanitizer) { }
+  gcsesearch: SafeHtml;
   ngOnInit() {
+    this.gcsesearch = this.sanitizer.bypassSecurityTrustHtml("<gcse:search></gcse:search>");
+      var cx = '012556502739128545047:0ugxtgpijge';
+    var gcse = document.createElement('script');
+    gcse.type = 'text/javascript';
+    gcse.async = true;
+    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(gcse, s);
   }
-
 }
